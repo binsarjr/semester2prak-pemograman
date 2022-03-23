76d7a56044e43b24a3050fa0e6fdc4699ebf2228
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-03-23 15:18:00 +0700

    Split code

---
+++ prak1/Car.cs
@@ -0,0 +1,16 @@
+class Car
+{
+    //String color = "red";
+    // int transmissionState = 0;
+    // Double speedMax = 250;
+    public Double speedCurrent = 0;
+    public void go()
+    {
+        speedCurrent += 10;
+    }
+
+    public void turnLeft()
+    {
+        speedCurrent -= 2;
+    }
+}
--- prak1/motor.cs
+++ prak1/motor.cs
@@ -2,38 +2,23 @@ using System;
 
 namespace Project
 {
-    class MotorSport
-    {
-        //String color = "red";
-        // int transmissionState = 0;
-        // Double speedMax = 250;
-        public Double speedCurrent = 0;
-        public void go()
-        {
-            speedCurrent += 10;
-        }
 
-        public void turnLeft()
-        {
-            speedCurrent -= 2;
-        }
-    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            MotorSport motorSport = new MotorSport();
+            Car lamborghini = new Car();
             Console.WriteLine("current speed : " +
-                              motorSport.speedCurrent);
+                              lamborghini.speedCurrent);
             motorSport.go();//speed = 10
             Console.WriteLine("current speed : " +
-                              motorSport.speedCurrent);
+                              lamborghini.speedCurrent);
             motorSport.turnLeft();//speed = 8
             Console.WriteLine("current speed : " +
-                              motorSport.speedCurrent);
+                              lamborghini.speedCurrent);
             motorSport.go();//speed = 18
             Console.WriteLine("current speed : " +
-                              motorSport.speedCurrent);
+                              lamborghini.speedCurrent);
         }
     }
 }
