d93ec53373fec8d14858cfdbf2b7e3e4c58337e2
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-04-06 14:00:49 +0700

    add withMilk builder

--- prak4/Program.cs
+++ prak4/Program.cs
@@ -1,6 +1,7 @@
 using System;
 namespace TheCoffeMechine
 {
+
     class Program
     {
         static void Main(string[] args)
@@ -9,7 +10,7 @@ namespace TheCoffeMechine
             WaterGalon water = new WaterGalon(1000);
             Milk milk = new Milk(1000);
             CoffeMechine yummyCoffe = new CoffeMechine.Builder(coffe, water)
-              
+                .withMilk(milk)                             
                 .build();
             Console.WriteLine("check " + yummyCoffe.checkAvailability());
             String result = yummyCoffe.makeCappucino();
